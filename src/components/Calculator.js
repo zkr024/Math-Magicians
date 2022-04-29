bf40ba0ff14a4bd019b6d28f715e85111dda1075
zkr024 <zkr.bm.02403@gmail.com>
2022-04-29 15:11:27 -0500

    replace class with a function

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,54 +1,50 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import calculate from '../logic/calculate';
 
-class Calculator extends Component {
-  constructor(props) {
-    super(props);
+const Calculator = () => {
+  const [details, setDetails] = useState({
+    total: null,
+    next: null,
+    operation: null,
+  });
 
-    this.state = {
-      total: null,
-      next: null,
-      operation: null,
-    };
-  }
+  const { total } = details;
+  const { next } = details;
+  const { operation } = details;
 
-  render() {
-    let information = {};
-    const value = (e) => {
-      const data = e.target.innerHTML;
-      information = calculate(this.state, data);
-      this.setState(information);
-    };
+  let elements = {};
+  const value = (e) => {
+    const data = e.target.innerHTML;
+    elements = calculate(details, data);
+    setDetails(elements);
+  };
 
-    const { total, next, operation } = this.state;
-
-    return (
-      <div className="calculator-grid">
-        <div className="output">
-          <div className="content">{total !== null && operation == null ? total : next}</div>
-        </div>
-        <button type="button" onClick={value}>AC</button>
-        <button type="button" onClick={value}>+/-</button>
-        <button type="button" onClick={value}>%</button>
-        <button type="button" onClick={value} className="operator">÷</button>
-        <button type="button" onClick={value}>7</button>
-        <button type="button" onClick={value}>8</button>
-        <button type="button" onClick={value}>9</button>
-        <button type="button" onClick={value} className="operator">x</button>
-        <button type="button" onClick={value}>4</button>
-        <button type="button" onClick={value}>5</button>
-        <button type="button" onClick={value}>6</button>
-        <button type="button" onClick={value} className="operator">-</button>
-        <button type="button" onClick={value}>1</button>
-        <button type="button" onClick={value}>2</button>
-        <button type="button" onClick={value}>3</button>
-        <button type="button" onClick={value} className="operator">+</button>
-        <button type="button" onClick={value} className="zero">0</button>
-        <button type="button" onClick={value}>.</button>
-        <button type="button" onClick={value} className="operator">=</button>
+  return (
+    <div className="calculator-grid">
+      <div className="output">
+        <div className="content">{total !== null && operation == null ? total : next}</div>
       </div>
-    );
-  }
-}
+      <button type="button" onClick={value}>AC</button>
+      <button type="button" onClick={value}>+/-</button>
+      <button type="button" onClick={value}>%</button>
+      <button type="button" onClick={value} className="operator">÷</button>
+      <button type="button" onClick={value}>7</button>
+      <button type="button" onClick={value}>8</button>
+      <button type="button" onClick={value}>9</button>
+      <button type="button" onClick={value} className="operator">x</button>
+      <button type="button" onClick={value}>4</button>
+      <button type="button" onClick={value}>5</button>
+      <button type="button" onClick={value}>6</button>
+      <button type="button" onClick={value} className="operator">-</button>
+      <button type="button" onClick={value}>1</button>
+      <button type="button" onClick={value}>2</button>
+      <button type="button" onClick={value}>3</button>
+      <button type="button" onClick={value} className="operator">+</button>
+      <button type="button" onClick={value} className="zero">0</button>
+      <button type="button" onClick={value}>.</button>
+      <button type="button" onClick={value} className="operator">=</button>
+    </div>
+  );
+};
 
 export default Calculator;
