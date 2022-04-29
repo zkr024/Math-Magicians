32607274fdc1bbc5b63861fba699aefb3300f42a
zkr024 <zkr.bm.02403@gmail.com>
2022-04-29 12:18:21 -0500

    Link logic files

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,32 +1,51 @@
 import React, { Component } from 'react';
+import calculate from '../logic/calculate';
 
-// eslint-disable-next-line react/prefer-stateless-function
 class Calculator extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      total: null,
+      next: null,
+      operation: null,
+    };
+  }
+
   render() {
+    let information = {};
+    const value = (e) => {
+      const data = e.target.innerHTML;
+      information = calculate(this.state, data);
+      this.setState(information);
+    };
+
+    const { total, next, operation } = this.state;
+
     return (
       <div className="calculator-grid">
         <div className="output">
-          <div className="content">0</div>
+          <div className="content">{total !== null && operation == null ? total : next}</div>
         </div>
-        <button type="button">AC</button>
-        <button type="button">+/-</button>
-        <button type="button">%</button>
-        <button type="button" className="operator">÷</button>
-        <button type="button">7</button>
-        <button type="button">8</button>
-        <button type="button">9</button>
-        <button type="button" className="operator">X</button>
-        <button type="button">4</button>
-        <button type="button">5</button>
-        <button type="button">6</button>
-        <button type="button" className="operator">-</button>
-        <button type="button">1</button>
-        <button type="button">2</button>
-        <button type="button">3</button>
-        <button type="button" className="operator">+</button>
-        <button type="button" className="zero">0</button>
-        <button type="button">.</button>
-        <button type="button" className="operator">=</button>
+        <button type="button" onClick={value}>AC</button>
+        <button type="button" onClick={value}>+/-</button>
+        <button type="button" onClick={value}>%</button>
+        <button type="button" onClick={value} className="operator">÷</button>
+        <button type="button" onClick={value}>7</button>
+        <button type="button" onClick={value}>8</button>
+        <button type="button" onClick={value}>9</button>
+        <button type="button" onClick={value} className="operator">x</button>
+        <button type="button" onClick={value}>4</button>
+        <button type="button" onClick={value}>5</button>
+        <button type="button" onClick={value}>6</button>
+        <button type="button" onClick={value} className="operator">-</button>
+        <button type="button" onClick={value}>1</button>
+        <button type="button" onClick={value}>2</button>
+        <button type="button" onClick={value}>3</button>
+        <button type="button" onClick={value} className="operator">+</button>
+        <button type="button" onClick={value} className="zero">0</button>
+        <button type="button" onClick={value}>.</button>
+        <button type="button" onClick={value} className="operator">=</button>
       </div>
     );
   }
