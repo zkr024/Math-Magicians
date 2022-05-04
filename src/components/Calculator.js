ef646a40ba968fe4058233a3fe4cc36f0b6f0c50
zkr024 <zkr.bm.02403@gmail.com>
2022-05-04 10:42:22 -0500

    Add some design to the pages

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import calculate from '../logic/calculate';
+import './Calculator.css';
 
 const Calculator = () => {
   const [details, setDetails] = useState({
@@ -20,29 +21,34 @@ const Calculator = () => {
   };
 
   return (
-    <div className="calculator-grid">
-      <div className="output">
-        <div className="content">{total !== null && operation == null ? total : next}</div>
+    <div className="container">
+      <h2 className="title">Let&apos;s do some math</h2>
+      <div className="calculator">
+        <div className="calculator-grid">
+          <div className="output">
+            <div className="content">{total !== null && operation == null ? total : next}</div>
+          </div>
+          <button type="button" onClick={value}>AC</button>
+          <button type="button" onClick={value}>+/-</button>
+          <button type="button" onClick={value}>%</button>
+          <button type="button" onClick={value} className="operator">÷</button>
+          <button type="button" onClick={value}>7</button>
+          <button type="button" onClick={value}>8</button>
+          <button type="button" onClick={value}>9</button>
+          <button type="button" onClick={value} className="operator">x</button>
+          <button type="button" onClick={value}>4</button>
+          <button type="button" onClick={value}>5</button>
+          <button type="button" onClick={value}>6</button>
+          <button type="button" onClick={value} className="operator">-</button>
+          <button type="button" onClick={value}>1</button>
+          <button type="button" onClick={value}>2</button>
+          <button type="button" onClick={value}>3</button>
+          <button type="button" onClick={value} className="operator">+</button>
+          <button type="button" onClick={value} className="zero">0</button>
+          <button type="button" onClick={value}>.</button>
+          <button type="button" onClick={value} className="operator">=</button>
+        </div>
       </div>
-      <button type="button" onClick={value}>AC</button>
-      <button type="button" onClick={value}>+/-</button>
-      <button type="button" onClick={value}>%</button>
-      <button type="button" onClick={value} className="operator">÷</button>
-      <button type="button" onClick={value}>7</button>
-      <button type="button" onClick={value}>8</button>
-      <button type="button" onClick={value}>9</button>
-      <button type="button" onClick={value} className="operator">x</button>
-      <button type="button" onClick={value}>4</button>
-      <button type="button" onClick={value}>5</button>
-      <button type="button" onClick={value}>6</button>
-      <button type="button" onClick={value} className="operator">-</button>
-      <button type="button" onClick={value}>1</button>
-      <button type="button" onClick={value}>2</button>
-      <button type="button" onClick={value}>3</button>
-      <button type="button" onClick={value} className="operator">+</button>
-      <button type="button" onClick={value} className="zero">0</button>
-      <button type="button" onClick={value}>.</button>
-      <button type="button" onClick={value} className="operator">=</button>
     </div>
   );
 };
