bbe60eacface67c95afad2bacffc1630d6d6c963
Abdul Hadi Jafari <abdulhadijafari2015@gmail.com>
2022-05-05 19:44:10 +0430

    Fix eslint error

--- src/logic/__test__/calculate.test.js
+++ src/logic/__test__/calculate.test.js
@@ -1,51 +1,51 @@
-import calculate from "../calculate";
+import calculate from '../calculate';
 
-it("expect next to be equal to 4", () => {
+it('expect next to be equal to 4', () => {
   const nxtTesting = {
     total: null,
     next: null,
   };
-  expect(calculate(nxtTesting, "4")).toEqual({ next: "4", total: null });
+  expect(calculate(nxtTesting, '4')).toEqual({ next: '4', total: null });
 });
 
-it("expect operation to be +", () => {
+it('expect operation to be +', () => {
   const plusTesting = {
-    total: "4",
-    operation: "+",
+    total: '4',
+    operation: '+',
     next: null,
   };
-  expect(calculate(plusTesting, "+")).toEqual(plusTesting);
+  expect(calculate(plusTesting, '+')).toEqual(plusTesting);
 });
 
-it("expect total to be 10", () => {
+it('expect total to be 10', () => {
   const totalTesting = {
-    total: "10",
-    operation: "+",
+    total: '10',
+    operation: '+',
     next: null,
   };
-  expect(calculate(totalTesting, "5")).toEqual({ total: "10", operation: "+", next: "5" });
+  expect(calculate(totalTesting, '5')).toEqual({ total: '10', operation: '+', next: '5' });
 });
 
-it("should display Total", () => {
+it('should display Total', () => {
   const displayTotal = {
-    total: "10",
-    operation: "+",
-    next: "9",
+    total: '10',
+    operation: '+',
+    next: '9',
   };
-  expect(calculate(displayTotal, "=")).toEqual({
-    total: "19",
+  expect(calculate(displayTotal, '=')).toEqual({
+    total: '19',
     operation: null,
     next: null,
   });
 });
 
-it("Should clear the screen ", () => {
+it('Should clear the screen ', () => {
   const clear = {
-    total: "10",
-    operation: "+",
-    next: "9",
+    total: '10',
+    operation: '+',
+    next: '9',
   };
-  expect(calculate(clear, "AC")).toEqual({
+  expect(calculate(clear, 'AC')).toEqual({
     total: null,
     operation: null,
     next: null,
